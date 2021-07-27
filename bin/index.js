fbc033412391d67af3ab42a802d0cdbea2f16e03
chen.shuhang <437132124@qq.com>
2021-07-27 17:48:20 +0800

    feat: test dumi

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,18 @@
-const { program } = require("commander");
-const color = require("colors-cli");
+const { program } = require('commander');
+const color = require('colors-cli');
 
 const commanderAction = (command, type) => {
-  console.log('\n\n this is first command', color.blue(command), color.green(type));
-}
+  console.log(
+    '\n\n this is first command',
+    color.blue(command),
+    color.green(type),
+    color.red('version: 0.0.2'),
+  );
+};
 
 /* 必选参数  alita框架名称 type组件/还是页面 */
 program
-  .version("1.1.0", "-v --version -V")
-  .arguments("<command> <type>")
+  .version('0.0.2', '-v --version -V')
+  .arguments('<command> <type>')
   .action(commanderAction)
   .parse(process.argv);
---
+++ src/components/Foo/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface FooProps {}
+
+const Foo: FC<FooProps> = () => {
+  return <div>123</div>;
+};
+
+export default Foo;
